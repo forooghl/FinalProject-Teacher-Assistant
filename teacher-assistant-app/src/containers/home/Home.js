bd9b53377febae3b5330cfb74addc9581ac40d28
forooghL <foroogh.msl@gmail.com>
2024-02-26 16:20:57 +0330

    props of cardItem file changed

--- teacher-assistant-app/src/containers/home/Home.js
+++ teacher-assistant-app/src/containers/home/Home.js
@@ -1,24 +1,44 @@
 import { React } from "react";
 import Navbar from "../../component/Navbar/Navbar";
-import Course from "../../component/Course/Course";
+import CardItem from "../../component/CardItem/CardItem";
 import Card from "../../component/Card/Card";
 
 const Home = () => {
     // fake data for testing UI
     const course = (
         <>
-            <Course semester="پاییز 1402" teacherName="دکتر علی غلامی رودی" courseName="کامپایلر" />
-            <Course semester="بهار 1402" teacherName="دکتر علی غلامی رودی" courseName="سیستم عامل" />
+            <CardItem
+                date="پاییز 1402"
+                teacherName="دکتر علی غلامی رودی"
+                title="کامپایلر"
+                id="1"
+                linkURL="course"
+            />
+            <CardItem
+                date="بهار 1402"
+                teacherName="دکتر علی غلامی رودی"
+                title="سیستم عامل"
+                id="2"
+                linkURL="course"
+            />
         </>
     );
 
-    const TaCourse = <Course semester="پاییز 1401" teacherName="دکتر علی غلامی رودی" courseName="سیستم عامل" />;
+    const TaCourse = (
+        <CardItem
+            date="پاییز 1401"
+            teacherName="دکتر علی غلامی رودی"
+            title="سیستم عامل"
+            id="3"
+            linkURL="course"
+        />
+    );
     return (
         <>
             <Navbar />
             <div className="flex justify-evenly flex-wrap max-md:flex-nowrap mt-10 max-md:flex-col max-md:items-center ">
-                <Card title="کلاس های من" courses={course} />
-                <Card title="کلاس های درس" courses={TaCourse} />
+                <Card title="کلاس های من" items={course} />
+                <Card title="کلاس های درس" items={TaCourse} />
             </div>
         </>
     );
