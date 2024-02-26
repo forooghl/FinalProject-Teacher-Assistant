d3e09714bf5a49d3a5d7da98831152ad860e620c
forooghL <foroogh.msl@gmail.com>
2024-02-26 16:20:09 +0330

    course page route added

--- teacher-assistant-app/src/containers/Routers.js
+++ teacher-assistant-app/src/containers/Routers.js
@@ -5,12 +5,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./auth/Login/Login";
 import Signup from "./auth/Signup/Signup";
 import Home from "./home/Home";
+import CourseView from "./courseView/CourseView";
 
 class Routers extends React.Component {
     render() {
         return (
             <Routes>
                 <Route path="/" exact element={<Home />} />
+                <Route path="/course/:id" exact element={<CourseView />} />
                 <Route path="/login" exact element={<Login />} />
                 <Route path="/signup" exact element={<Signup />} />
             </Routes>
