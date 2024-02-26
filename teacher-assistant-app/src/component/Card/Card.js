0d3026913f0c2a0cee94bf6bfb08c40e685ec1bc
forooghL <foroogh.msl@gmail.com>
2024-02-26 16:17:09 +0330

    props changed

--- teacher-assistant-app/src/component/Card/Card.js
+++ teacher-assistant-app/src/component/Card/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     return (
         <div className="flex flex-col w-2/5 max-md:w-10/12">
             <p className="font-bold text-raisin-black text-lg pr-3 mb-2">{props.title}</p>
-            <div className="border-solid border border-rich-black-fogra-29/10 shadow rounded-md max-md:mb-8">{props.courses}</div>
+            <div className="border-solid border border-rich-black-fogra-29/10 shadow rounded-md max-md:mb-8">{props.items}</div>
         </div>
     );
 };
