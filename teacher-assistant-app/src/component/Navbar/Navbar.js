80465fd3de3673d75bedcc66330558049c1e1393
forooghL <foroogh.msl@gmail.com>
2024-02-26 16:16:25 +0330

    class changed to className :)

--- teacher-assistant-app/src/component/Navbar/Navbar.js
+++ teacher-assistant-app/src/component/Navbar/Navbar.js
@@ -8,17 +8,17 @@ const Navbar = (props) => {
     // TODO : control activeClassName="is-active" in NavLink with props
     return (
         <header>
-            <nav class="bg-cultured/50 shadow shadow-independence/15 w-100 px-8 md:px-auto">
-                <div class="h-16 mx-auto md:px-4 container flex items-center justify-between flex-nowrap">
+            <nav className="bg-cultured/50 shadow shadow-independence/15 w-100 px-8 md:px-auto">
+                <div className="h-16 mx-auto md:px-4 container flex items-center justify-between flex-nowrap">
                     {/* Logo */}
                     <div>
                         <NavLink to="/">
-                            <img class="h-11 w-11" src={Logo} />
+                            <img className="h-11 w-11" src={Logo} />
                         </NavLink>
                     </div>
-                    <div class="text-tufts-blue basis-11/12 max-md:w-auto max-md:basis-1/3">
-                        <ul class="flex font-semibold justify-start max-md:justify-between">
-                            <li class="md:px-4 md:py-2 text-queen-blue font-bold">
+                    <div className="text-tufts-blue basis-11/12 max-md:w-auto max-md:basis-1/3">
+                        <ul className="flex font-semibold justify-start max-md:justify-between">
+                            <li className="md:px-4 md:py-2 text-queen-blue font-bold">
                                 <NavLink to="/">داشبورد</NavLink>
                             </li>
                             <li class="md:px-4 md:py-2 hover:text-blue-yonder">
@@ -26,7 +26,7 @@ const Navbar = (props) => {
                             </li>
                         </ul>
                     </div>
-                    <div class="text-queen-blue max-md:w-fit">
+                    <div className="text-queen-blue max-md:w-fit">
                         {username ? (
                             <NavLink to="/">
                                 <div className="fa-layers fa-fw fa-2xl hover:text-blue-yonder">
@@ -35,9 +35,9 @@ const Navbar = (props) => {
                             </NavLink>
                         ) : (
                             <NavLink to="/login">
-                                <div class="px-3 py-1 bg-queen-blue hover:bg-blue-yonder text-cultured rounded-lg flex items-center gap-2">
+                                <div className="px-3 py-1 bg-queen-blue hover:bg-blue-yonder text-cultured rounded-lg flex items-center gap-2">
                                     <span>ورود</span>
-                                    <i class="fa-solid fa-arrow-right-to-bracket"></i>
+                                    <i className="fa-solid fa-arrow-right-to-bracket"></i>
                                 </div>
                             </NavLink>
                         )}
