b84a2795a469723e57b73bbf1349157289ba558e
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:25:03 +0330

    connected to the backend

--- teacher-assistant-app/src/containers/courseView/CourseView.js
+++ teacher-assistant-app/src/containers/courseView/CourseView.js
@@ -1,53 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../component/Navbar/Navbar";
 import Card from "../../component/Card/Card";
 import CardItem from "../../component/CardItem/CardItem";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const CourseView = (props) => {
-    // fake data for testing UI
-    const practice = (
-        <>
-            <CardItem title="تمرین سری سوم" teacherName="" date="23 فروردین" id="3" linkURL="practice" />
-            <CardItem title="تمرین سری دوم" teacherName="" date="23 اسفند" id="2" linkURL="practice" />
-            <CardItem title="تمرین سری اول" teacherName="" date="23 بهمن" id="1" linkURL="practice" />
-        </>
-    );
-    const classDetail = (
-        <div className="text-center text-raisin-black mt-2">
-            {/* dynamic data when connected to the backend */}
-            <h1 className="font-bold text-xl mb-2">اصول طراحی کامپایلر</h1>
-            <h2 className=" text-sm">دکتر علی غلامی رودی</h2>
+    const { id } = useParams();
 
-            <div class="flex items-center px-4 my-2">
-                <div class="flex-1 border-t-2 border-independece/15"></div>
-                <span class="px-3 font-bold">دستیاران آموزشی</span>
-                <div class="flex-1 border-t-2 border-independece/15"></div>
-            </div>
+    const token = localStorage.getItem("token");
+    const username = sessionStorage.getItem("username");
 
-            {/* dynamic data when connected to the backend */}
-            <div className="text-sm">
-                <p className="mb-2">محدثه قنبری</p>
-                <p className="mb-2">فاطمه زحمتکش</p>
-                <p className="mb-2">فروغ میراسماعیلی</p>
+    const [course, setCourse] = useState([]);
+    const [exercise, setExercise] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                setIsLoading(true);
+                const response = await axios.get(`http://127.0.0.1:8000/courses/courseData/${id}`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                setCourse(response.data.course_data);
+                setIsLoading(false);
+            } catch (error) {
+                Promise.reject(error);
+                // navigate("/error404");
+            }
+
+            try {
+                setIsLoading(true);
+                const response = await axios.get(`http://127.0.0.1:8000/courses/courseExercise/${id}`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                setExercise(response.data.course_exercises);
+                setIsLoading(false);
+            } catch (error) {
+                Promise.reject(error);
+                // navigate("/error404");
+            }
+        }
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <Navbar />;
+    } else {
+        const practice = (
+            <>
+                {exercise.map((item) => {
+                    return (
+                        <CardItem
+                            title={item.exerciseName}
+                            teacherName=""
+                            date={new Date(item.endDate).toLocaleString().split(',')[0]}
+                            id={item.id}
+                            linkURL="practice"
+                        />
+                    );
+                })}
+            </>
+        );
+        const classDetail = (
+            <div className="text-center text-raisin-black mt-2">
+                <h1 className="font-bold text-xl mb-2">{course[0].courseName}</h1>
+                <h2 className=" text-sm">{course[0].professor}</h2>
+
+                <div class="flex items-center px-4 my-2">
+                    <div class="flex-1 border-t-2 border-independece/15"></div>
+                    <span class="px-3 font-bold">دستیاران آموزشی</span>
+                    <div class="flex-1 border-t-2 border-independece/15"></div>
+                </div>
+
+                <div className="text-sm">
+                    {course[0].Ta.map((items) => {
+                        return <p className="mb-2">{items}</p>;
+                    })}
+                </div>
             </div>
-        </div>
-    );
-    return (
-        <>
-            <Navbar />
-            <div className="flex justify-evenly flex-wrap max-md:flex-nowrap mt-10 max-md:flex-col max-md:items-center">
-                <div className="w-3/12  max-md:w-10/12">
-                    <Card title="🏛️درس در یک نگاه" items={classDetail} />
+        );
+        return (
+            <>
+                <Navbar />
+                <div className="flex justify-evenly flex-wrap max-md:flex-nowrap mt-10 max-md:flex-col max-md:items-center">
+                    <div className="w-3/12  max-md:w-10/12">
+                        <Card title="🏛️درس در یک نگاه" items={classDetail} />
+                    </div>
+                    <div className="w-3/12  max-md:w-10/12">
+                        <Card title="📰 اطلاع رسانی" items={practice} />
+                    </div>
+                    <div className="w-4/12  max-md:w-10/12">
+                        <Card title="📑تمرین‌ها" items={practice} />
+                    </div>
                 </div>
-                <div className="w-3/12  max-md:w-10/12">
-                    <Card title="📰 اطلاع رسانی" items={practice} />
-                </div>
-                <div className="w-4/12  max-md:w-10/12">
-                    <Card title="📑تمرین‌ها" items={practice} />
-                </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 };
 
 export default CourseView;
--- teacher-assistant-app/src/containers/exercise/Exercise.js
+++ teacher-assistant-app/src/containers/exercise/Exercise.js
@@ -1,42 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../component/Navbar/Navbar";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+const Exercise = (props) => {
+    const { id } = useParams();
 
-const Exercise = () => {
-    return (
-        <>
-            <Navbar />
-            <div className="flex w-5/12 max-md:w-4/5 mx-auto my-10 py-4 px-4 shadow shadow-independence/15 flex-wrap md:flex-nowrap ">
-                <div className="flex flex-col">
-                    <h1 className="font-bold text-raisin-black text-lg self-center mb-2">تمرین سری یک</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores exercitationem excepturi
-                        porro nesciunt fugit culpa distinctio error doloremque, dignissimos cupiditate consequuntur
-                        eaque et beatae odit quis cum, quam possimus placeat eius officia laborum eum. Optio?Lorem ipsum
-                        dolor sit amet, consectetur adipisicing elit. Asperiores exercitationem excepturi porro nesciunt
-                        fugit culpa distinctio error doloremque, dignissimos cupiditate consequuntur eaque et beatae
-                        odit quis cum, quam possimus placeat eius officia laborum eum. Optio?
-                    </p>
-                    <div class="flex items-center px-4 my-4">
-                        <div class="flex-1 border-t-2 border-independece/15"></div>
-                        <span class="px-3 font-bold">بارگذاری پاسخ تمرین</span>
-                        <div class="flex-1 border-t-2 border-independece/15"></div>
+    const token = localStorage.getItem("token");
+    const username = sessionStorage.getItem("username");
+
+    const [exercise, setExercise] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                setIsLoading(true);
+                const response = await axios.get(`http://127.0.0.1:8000/courses/exerciseData/${id}`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                setExercise(response.data.exercise_data);
+                setIsLoading(false);
+            } catch (error) {
+                Promise.reject(error);
+                // navigate("/error404");
+            }
+        }
+        fetchData();
+    }, []);
+
+    // exercise file is not displayed
+    if (isLoading) {
+        return <Navbar />;
+    } else {
+        return (
+            <>
+                <Navbar />
+                <div className="flex w-5/12 max-md:w-4/5 mx-auto my-10 py-4 px-4 shadow shadow-independence/15 flex-wrap md:flex-nowrap ">
+                    <div className="flex flex-col w-full">
+                        <h1 className="font-bold text-raisin-black text-lg self-center mb-2">{exercise[0].exerciseName}</h1>
+                        <p>
+                            {exercise[0].body}
+                        </p>
+                        <div class="flex items-center px-4 my-4">
+                            <div class="flex-1 border-t-2 border-independece/15"></div>
+                            <span class="px-3 font-bold">بارگذاری پاسخ تمرین</span>
+                            <div class="flex-1 border-t-2 border-independece/15"></div>
+                        </div>
+
+                        <form>
+                            <label htmlFor="uploadFile" className="block">
+                                فایل <span className="text-red-500">*</span>
+                            </label>
+                            {/* upload is not working :) */}
+                            <input
+                                id="uploadFile"
+                                name="uploadFile"
+                                type="file"
+                                className="text-raisin-black/50 cursor-pointer file:cursor-pointer file:border-none file:py-1 file:px-3 file:bg-queen-blue file:hover:bg-blue-yonder file:text-cultured file:rounded"
+                            />
+                        </form>
                     </div>
-
-                    <form>
-                        <label htmlFor="uploadFile" className="block">
-                            فایل <span className="text-red-500">*</span>
-                        </label>
-                        <input
-                            id="uploadFile"
-                            name="uploadFile"
-                            type="file"
-                            className="text-raisin-black/50 cursor-pointer file:cursor-pointer file:border-none file:py-1 file:px-3 file:bg-queen-blue file:hover:bg-blue-yonder file:text-cultured file:rounded"
-                        />
-                    </form>
                 </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 };
 
 export default Exercise;
