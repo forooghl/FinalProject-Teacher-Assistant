0a32ac80efd0dc96c12767851a3b23de84993c40
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:14:33 +0330

    fullname Field added to models

--- Authentication/models.py
+++ Authentication/models.py
@@ -26,6 +26,7 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
+    fullName = models.CharField(max_length=255, null=True)
     avatar = models.URLField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -36,4 +37,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['email']
 
     def __str__(self):
-        return self.email
+        if self.fullName == None:
+            return self.username
+        else:
+            return self.fullName
