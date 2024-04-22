3a9438ce87a4621851af18adab7c4f5aed9ef485
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:23:09 +0330

    added new urls

--- Students/urls.py
+++ Students/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import addNewClass, uploadExerciseAns
+from .views import addNewClass, uploadExerciseAns, myClass
 urlpatterns = [
     path('addNewClass/', addNewClass.as_view()),
-    path('uploadExercise/', uploadExerciseAns.as_view())
+    path('uploadExercise/', uploadExerciseAns.as_view()),
+    path('myClasses/', myClass.as_view())
 ]
