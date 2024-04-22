9198f9d756b78ea8e40806d425087570379324dc
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:17:57 +0330

    added new serializer for getting exercise of course

--- Courses/serializers.py
+++ Courses/serializers.py
@@ -3,11 +3,18 @@ from .models import Course, Exercise
 
 
 class CourseSerializers(serializers.ModelSerializer):
+    professor = serializers.SlugRelatedField(slug_field='username', read_only = True)
+    Ta = serializers.StringRelatedField(many=True)
     class Meta:
         model = Course
         fields = '__all__'
         
 class ExerciseSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ['id', 'exerciseName', 'endDate']
+        
+class ExerciseDataSerializers(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = '__all__'
