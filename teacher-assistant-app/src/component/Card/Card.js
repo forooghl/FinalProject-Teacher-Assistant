6662f07bc55aeec9c7192847ec317801ed297d6f
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:26:56 +0330

    add a new div if there is no info to display

--- teacher-assistant-app/src/component/Card/Card.js
+++ teacher-assistant-app/src/component/Card/Card.js
@@ -4,7 +4,15 @@ const Card = (props) => {
     return (
         <div className="flex flex-col">
             <p className="font-bold text-raisin-black text-lg pr-3 mb-2">{props.title}</p>
-            <div className="border-solid border border-rich-black-fogra-29/10 shadow rounded-md max-md:mb-8">{props.items}</div>
+            {props.items.props.children.length < 1 ? (
+                <div className="border-solid border border-rich-black-fogra-29/10 py-2 text-center shadow rounded-md max-md:mb-8">
+                    چیزی برای نمایش وجود ندارد
+                </div>
+            ) : (
+                <div className="border-solid border border-rich-black-fogra-29/10 shadow rounded-md max-md:mb-8">
+                    {props.items}
+                </div>
+            )}
         </div>
     );
 };
