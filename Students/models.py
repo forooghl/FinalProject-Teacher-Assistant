251e8995eb689f79bc25cac6a13a8debd832a44e
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:21:03 +0330

    fix issue : unique error

--- Students/models.py
+++ Students/models.py
@@ -7,16 +7,12 @@ from Authentication.models import UserProfile
 from Courses.models import Course, Exercise
 
 class StudentCourses(models.Model):
-    std_number = models.IntegerField(unique=True)
-    user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, unique=True)
+    std_number = models.IntegerField()
+    user_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['user_id', 'std_number'], name='unique_std')
-        ]
+
 
 class StdExercise(models.Model):
     exercise_id = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(StudentCourses, to_field='user_id', on_delete=models.CASCADE, related_name='exercises_submitted')
-    std_id = models.ForeignKey(StudentCourses, to_field='std_number', on_delete=models.CASCADE, related_name='student_exercises')
+    std_course_id = models.ForeignKey(StudentCourses, on_delete=models.CASCADE, related_name='exercises_submitted')
     file = models.FileField(upload_to='ExerciseAns')
