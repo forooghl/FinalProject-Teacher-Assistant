a7c975fa0f252eb2a1783b07258cbb4f60566a9a
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:22:44 +0330

    add new view for get student all class data

--- Students/views.py
+++ Students/views.py
@@ -8,9 +8,27 @@ from rest_framework.response import Response
 
 from Authentication.models import UserProfile
 from Authentication.serializers import UserSerializer
+from Courses.models import Course
+from Courses.serializers import CourseSerializers
 
 from .models import StudentCourses
-from .serializers import StdCourseSerializers, StdExerciseSerializers
+from .serializers import StdCourseSerializers, StdExerciseSerializers, myCourseSerializers
+
+class myClass(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request):
+        try:
+            serializer = UserSerializer(request.user, many=False)
+            user_class = StudentCourses.objects.filter(user_id = serializer.data['id'])
+        except:
+            user_class = []
+        std_class = StdCourseSerializers(user_class, many = True)
+        serializer_class = myCourseSerializers(user_class, many = True)
+        my_course_id = [value for ordered_dict in serializer_class.data for value in ordered_dict.values()] 
+        class_data = Course.objects.filter(id__in = my_course_id)
+        course_serializer = CourseSerializers(class_data, many=True)
+
+        return Response({'classes' : std_class.data, 'course_data': course_serializer.data})
 
 class addNewClass(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
