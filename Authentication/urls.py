b3387f1e243eba80169f1818ce54547a81dadeb3
forooghL <foroogh.msl@gmail.com>
2024-04-22 18:15:52 +0330

    delete comment

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -11,15 +11,3 @@ urlpatterns = [
     path('logout/', LogoutAPIView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-# from . import views
-# from django.urls import path
-# from rest_framework_simplejwt.views import (
-#     TokenRefreshView,
-# )
-# app_name = 'authentication'
-# urlpatterns = [
-#     path('register/',views.RegisterView.as_view(),name="register"),
-#     path('login/',views.LoginAPIView.as_view(),name="login"),
-#     path('logout/', views.LogoutAPIView.as_view(), name="logout"),
-#     path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-# ]
