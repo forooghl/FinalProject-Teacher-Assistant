8efc39c029c126a0ea0c48780c321c5ad0239cfe
forooghL <foroogh.msl@gmail.com>
2024-05-03 16:32:40 +0330

    fix the add new course bug (added new serializers)

--- Courses/serializers.py
+++ Courses/serializers.py
@@ -3,6 +3,11 @@ from .models import Course, Exercise
 
 
 class CourseSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+        
+class CourseDataSerializers(serializers.ModelSerializer):
     professor = serializers.SlugRelatedField(slug_field='username', read_only = True)
     Ta = serializers.StringRelatedField(many=True)
     class Meta:
