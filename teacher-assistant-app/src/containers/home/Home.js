87b5d8170a23810a8f5c648bfe7a7b7ce9037eda
forooghL <foroogh.msl@gmail.com>
2024-05-08 17:59:57 +0330

    search bar added

--- teacher-assistant-app/src/containers/home/Home.js
+++ teacher-assistant-app/src/containers/home/Home.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../component/Navbar/Navbar";
 import CardItem from "../../component/CardItem/CardItem";
 import Card from "../../component/Card/Card";
-import axios from "axios";
+import axios from "../axios";
 
 const Home = () => {
     const username = sessionStorage.getItem("username");
@@ -13,6 +13,9 @@ const Home = () => {
     const [myClasses, setMyClasses] = useState([]);
     const [myTAClasses, setMyTAClasses] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [search, setSearch] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+
     useEffect(() => {
         async function fetchData() {
             if (username) {
@@ -38,15 +41,40 @@ const Home = () => {
                     Promise.reject(error);
                     // navigate("/error404");
                 }
-            }
-            else{
+            } else {
                 setIsLoading(false);
             }
         }
         fetchData();
     }, []);
 
+    const SearchResult = (event) => {
+        event.preventDefault();
+        axios
+            .get(`/courses/search?search=${search}`)
+            .then((request) => {
+                setSearchResult(request.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
     if (!isLoading) {
+        const searchCourse = (
+            <>
+                {searchResult.map((item) => {
+                    return (
+                        <CardItem
+                            date={new Date(item.date).toLocaleString().split(",")[0]}
+                            teacherName={item.professor}
+                            title={item.courseName}
+                            id={item.id}
+                            linkURL="course"
+                        />
+                    );
+                })}
+            </>
+        );
         const course = (
             <>
                 {myClasses.map((item) => {
@@ -82,6 +110,50 @@ const Home = () => {
                 <Navbar />
                 <div className="flex justify-evenly flex-wrap max-md:flex-nowrap mt-10 max-md:flex-col max-md:items-center ">
                     <div className="w-2/5  max-md:w-10/12">
+                        <form onSubmit={SearchResult} className="max-w-md mb-4">
+                            <div className="relative">
+                                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                    <svg
+                                        className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 20 20"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                        />
+                                    </svg>
+                                </div>
+                                <input
+                                    id="search"
+                                    type="search"
+                                    placeholder="نام درس مورد نظر را جست و جو کنید"
+                                    autoFocus
+                                    required
+                                    className="block w-full p-4 ps-10 text-sm text-raisin-black border placeholder:text-raisin-black-25 border-independence/25 rounded-lg bg-cultured focus:ring-independence/50 "
+                                    value={search}
+                                    onChange={(event) => {
+                                        setSearch(event.target.value);
+                                    }}
+                                />
+                                <button
+                                    type="submit"
+                                    className="text-white absolute end-2.5 bottom-2.5 bg-queen-blue hover:bg-blue-yonder focus:ring-4 focus:outline-none focus:ring-blue-yonder font-medium rounded-lg text-sm px-4 py-2"
+                                >
+                                    جست و جو
+                                </button>
+                            </div>
+                        </form>
+                        {searchResult.length >= 1 ? (
+                            <div className="mb-4">
+                                <Card title="نتیجه جست وجو" items={searchCourse} />
+                            </div>
+                        ) : <></>}
                         <Card title="کلاس های من" items={course} />
                     </div>
                     <div className="w-2/5  max-md:w-10/12">
