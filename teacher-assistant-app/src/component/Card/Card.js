a98e05ed04931c8c31a9533e2844a1e66869f9fd
forooghL <foroogh.msl@gmail.com>
2024-05-27 14:03:02 +0330

    fix an issue: empty TA class div condition

--- teacher-assistant-app/src/component/Card/Card.js
+++ teacher-assistant-app/src/component/Card/Card.js
@@ -1,6 +1,16 @@
 import { React } from "react";
 
 const Card = (props) => {
+    let empty
+    if (props.title == "کلاس های درس") {
+        if (props.items.props.children[0].length == 0 && props.items.props.children[1].length == 0) {
+            empty = 0;
+        } else {
+            empty = props.items.props.children[0].length + props.items.props.children[1].length;
+        }
+    } else {
+        empty = props.items.props.children.length;
+    }
     return (
         <div className="flex flex-col">
             <p className="font-bold text-raisin-black text-lg pr-3 mb-2">
@@ -11,7 +21,7 @@ const Card = (props) => {
                     <i className="fa fa-graduation-cap text-blue-yonder" aria-hidden="true"></i>
                 )}
             </p>
-            {props.items.props.children.length < 1 ? (
+            {empty < 1 ? (
                 <div className="border-solid border border-rich-black-fogra-29/10 py-2 text-center shadow rounded-md max-md:mb-8">
                     چیزی برای نمایش وجود ندارد
                 </div>
