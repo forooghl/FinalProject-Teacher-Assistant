7c23f83476317abeba440a8c007779fad79523ce
forooghL <foroogh.msl@gmail.com>
2024-06-11 13:44:58 +0330

    added url for TaEvaluation view

--- Students/urls.py
+++ Students/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import addNewClass, uploadExerciseAns, myAns, updateFinalAns, studentAns, myClass, joinOrNot
+from .views import addNewClass, uploadExerciseAns, myAns, updateFinalAns, studentAns, myClass, joinOrNot, taEvaluation
 urlpatterns = [
     path('addNewClass/', addNewClass.as_view()),
     path('uploadExercise/', uploadExerciseAns.as_view()),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('studentAnswer/', studentAns.as_view()),
     path('myClasses/', myClass.as_view()),
     path('joinOrNot/<id>', joinOrNot.as_view()),
+    path('evaluation/', taEvaluation.as_view()),
 ]
