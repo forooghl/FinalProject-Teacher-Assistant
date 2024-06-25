bdd9dbdf98ca280d20f74a44e0c155c20beeb500
forooghL <foroogh.msl@gmail.com>
2024-06-25 15:35:31 +0330

    fix an issue: default dir for profile picture was wrong

--- Authentication/models.py
+++ Authentication/models.py
@@ -27,7 +27,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
     fullName = models.CharField(max_length=255, null=True)
-    avatar = models.ImageField(default = 'default.png', upload_to = 'Profile', null=True , blank=True)
+    avatar = models.ImageField(default = 'Profile/default.png', upload_to = 'Profile', null=True , blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
