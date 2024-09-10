4e4a6b6ca9308976d086135e727852e447d4a323
forooghL <foroogh.msl@gmail.com>
2024-09-10 16:03:33 +0330

    added new fonts to the project

--- teacher-assistant-app/tailwind.config.js
+++ teacher-assistant-app/tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'raisin-black' : '#1a1c26ff',
-        'rich-black-fogra-29' : '#14151dff',
-        'independence' : '#4e5d7fff',
-        'queen-blue' : '#4f6388ff',
-        'blue-yonder' : '#416eaaff',
-        'tufts-blue' : '#5087c7ff',
-        'united-nations-blue' : '#5b8ecaff',
-        'cultured' : '#f2f3f5ff',
-      },
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "raisin-black": "#1a1c26ff",
+                "rich-black-fogra-29": "#14151dff",
+                independence: "#4e5d7fff",
+                "queen-blue": "#4f6388ff",
+                "blue-yonder": "#416eaaff",
+                "tufts-blue": "#5087c7ff",
+                "united-nations-blue": "#5b8ecaff",
+                cultured: "#f2f3f5ff",
+            },
+            fontFamily: {
+                Merienda: ["Merienda", "serif"],
+            },
+        },
     },
-    fontFamily: {
-      
-    },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
