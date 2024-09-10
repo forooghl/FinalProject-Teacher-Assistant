f1093d777ebc254cfbf124c2b504029912df56e4
forooghL <foroogh.msl@gmail.com>
2024-09-10 16:07:24 +0330

    added name to the project and few changes in style

--- teacher-assistant-app/src/component/Navbar/Navbar.js
+++ teacher-assistant-app/src/component/Navbar/Navbar.js
@@ -44,47 +44,77 @@ const Navbar = (props) => {
     return (
         <header>
             <nav className="bg-cultured/50 shadow shadow-independence/15 w-100 px-8 md:px-auto">
-                <div className="h-16 mx-auto md:px-4 container flex items-center justify-between flex-nowrap">
+                <div className="h-14 mx-auto md:px-4 container flex items-center justify-around flex-nowrap">
                     {/* Logo */}
                     <div>
                         <NavLink to="/">
-                            <img className="h-11 w-11" src={Logo} />
+                            <div className="flex flex-row justify-between">
+                                <img className="h-11 w-11" src={Logo} />
+                                <p className="self-center font-bold text-queen-blue font-Merienda">LearnEase</p>
+                            </div>
                         </NavLink>
                     </div>
-                    <div className="text-tufts-blue basis-8/12 max-lg:basis-6/12 max-md:w-auto max-md:basis-1/3">
+                    <div className="text-tufts-blue basis-9/12 max-lg:basis-6/12 max-md:w-auto max-md:basis-1/3">
                         <ul className="flex font-semibold justify-start max-md:justify-between">
-                            <li className="md:px-4 md:py-2 text-queen-blue font-bold">
-                                <NavLink to="/">داشبورد</NavLink>
+                            <li className="md:px-4 md:py-2">
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) =>
+                                        isActive
+                                            ? "ease-in duration-300 text-queen-blue font-bold pb-3 border-b-2 border-independence"
+                                            : "transition ease-in delay-150 duration-300 text-tufts-blue hover:text-queen-blue hover:font-bold "
+                                    }
+                                >
+                                    داشبورد
+                                </NavLink>
                             </li>
                             {isProfessor ? (
-                                <li className="md:px-4 md:py-2 hover:text-blue-yonder">
-                                    <NavLink to="/TaRS">پیشنهاد دستیار آموزشی</NavLink>
+                                <li className="md:px-4 md:py-2">
+                                    <NavLink
+                                        to="/TaRS"
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? "text-queen-blue font-bold pb-3 border-b-2 border-independence"
+                                                : "transition ease-in delay-150 duration-300 text-tufts-blue hover:text-queen-blue hover:font-bold "
+                                        }
+                                    >
+                                        پیشنهاد دستیار آموزشی
+                                    </NavLink>
                                 </li>
                             ) : (
                                 <></>
                             )}
-                            <li class="md:px-4 md:py-2 hover:text-blue-yonder">
-                                <NavLink href="/">ارتباط با ما</NavLink>
+                            <li class="md:px-4 md:py-2">
+                                <NavLink
+                                    to="/contact"
+                                    className={({ isActive }) =>
+                                        isActive
+                                            ? "text-queen-blue font-bold pb-3 border-b-2 border-independence"
+                                            : "transition ease-in delay-150 duration-300 text-tufts-blue hover:text-queen-blue hover:font-bold "
+                                    }
+                                >
+                                    ارتباط با ما
+                                </NavLink>
                             </li>
                         </ul>
                     </div>
                     <div className="text-queen-blue max-md:w-fit">
                         {username ? (
                             <NavLink to="/profile">
-                                <div className="flex flex-row ">
+                                <div className="flex flex-row transition ease-in delay-150 duration-300 hover:text-blue-yonder">
                                     <p className="py-4 px-4 font-semibold ">خوش آمدی {username}</p>
                                     {isLoading ? (
-                                        <div className="fa-layers fa-fw fa-2xl hover:text-blue-yonder">
+                                        <div className="fa-layers fa-fw fa-2xl">
                                             <i className="fa fa-user-circle" title="صفحه شخصی"></i>
                                         </div>
                                     ) : profile.avatar ? (
                                         <img
-                                            className=" w-12 h-12 rounded-full"
+                                            className=" w-12 h-12 rounded-full self-center"
                                             src={`http://127.0.0.1:8000${profile.avatar}`}
                                         />
                                     ) : (
                                         <img
-                                            className=" w-12 h-12 rounded-full"
+                                            className=" w-12 h-12 rounded-full self-center"
                                             src={`http://127.0.0.1:8000/media/Profile/default.png`}
                                         />
                                     )}
